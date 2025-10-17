9ed8975162b82857ef61f607db9efb659410dd96
BondarevskyYC <bondarevsky@yandex-team.ru>
2025-10-17 10:40:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// action/index.js
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-async function run() {
-  try {
-    core.info(`Hello, GitHub Action!`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run();
+console.log("привет мир из gh action")
